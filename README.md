4751c3929294b72aa0ee75f8a6027c73b3d96b04
Yoseph <yosephten@gmail.com>
2022-10-27 10:39:15 +0300

    file server setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # file-server
+
+`yarn install` for setup
+* Create a `/temp` folder, and setup your `.env` file before you begin
+`yarn start:dev` for the development server
+`yarn start` for production
+
+For Docker usage
+
+`docker build . -t file-server` then
+`docker run -p 3003:3003 -d file-server`
--- index.js
+++ index.js
@@ -1,36 +1,87 @@
 const express = require('express')
 const ftp = require("basic-ftp") 
+const fs = require('fs')
+const app = express()
+const port = 3003
+const CryptoJS = require("crypto-js");
+const dotenv = require('dotenv');
+dotenv.config(); // Configure to access .env files
 
-// const app = express()
-// const port = 3003
+// CLEAR /temp folder
+setInterval(() => {
+  fs.readdirSync(__dirname+'/temp').forEach(f => fs.rmSync(`${__dirname+'/temp'}/${f}`));
+}, 600000*12)
 
-example()
-
-async function example() {
-    const client = new ftp.Client()
-    client.ftp.verbose = true
-    try {
-        await client.access({
-            host: "127.0.0.1",
-            user: "yoseph",
-            password: "ftpserver",
-            secure: true,
-            secureOptions: { rejectUnauthorized: false }
-        })
-        console.log(await client.list())
+// Decryption
+function decryptedPath(cipher) {
+  try {
+    var decrypted = CryptoJS.Rabbit.decrypt(cipher, "12345678");
+    var originalText = decrypted.toString(CryptoJS.enc.Utf8);
+    var result = {
+      account: originalText.split('/')[0], 
+      project: originalText.split('/')[1], 
+      survey: originalText.split('/')[2], 
+      response: originalText.split('/')[3], 
+      id: originalText.split('/')[4]
     }
-    catch(err) {
-        console.log(err)
-    }
-    client.close()
+    return result
+  } catch (error) {
+    console.log(error)
+    return null 
+  }
 }
 
-// app.get('/', (req, res) => {
-//   var fileID = req.params.id;
-  
-//   res.send('Hello World! JUST EDITEDD')
-// })
+async function DOWNLOADITEM(account, project, survey, response, id) {
+  const client = new ftp.Client()
+  try {
+    await client.access({
+        host: process.env.FTP_ADDRESS,
+        user: process.env.FTP_USERNAME,
+        password: process.env.FTP_PASSWORD,
+        secure: true,
+        secureOptions: { rejectUnauthorized: false }
+    })
+    var result = await client.downloadTo("temp/"+id,`/accounts/${account}/${project}/${survey}/${response}/${id}`).catch((err) => console.log(err))
+    client.close()
+    return result;
+  }
+  catch(err) {
+      client.close()
+      return {code: -1}
+  }
+}
 
-// app.listen(port, () => {
-//   console.log(`File server listening on port ${port}`)
-// })
+app.use(express.static('temp'));
+
+app.get('/static/:path', async (req, res) => {
+  // Decrypt URL
+  const encryptedPath = req.params.path.replace('*', '/');
+  if (decryptedPath(encryptedPath) != null) {
+    var account = decryptedPath(encryptedPath).account;
+    var project = decryptedPath(encryptedPath).project;
+    var survey = decryptedPath(encryptedPath).survey;
+    var response = decryptedPath(encryptedPath).response;
+    var id = decryptedPath(encryptedPath).id;
+  } else
+    return res.status(500).send("Something went wrong")
+    
+  // Find in Cache
+  fs.readdirSync(__dirname+'/temp').forEach(file => {
+    if (id == file)
+      return res.status(200).sendFile('./temp/'+id, { root: __dirname });
+  });
+
+  try {
+    var result = await DOWNLOADITEM(account, project, survey, response, id) 
+    if (result.code >= 200 || result.code <= 300)
+      return res.status(200).sendFile('./temp/'+id, { root: __dirname });
+    else 
+      return res.status(500).send("Something went wrong")
+  } catch (error) {
+    return res.status(500).send("Something went wrong")
+  }
+})
+
+app.listen(port, () => {
+  console.log(`File server listening on port ${port}`)
+})
